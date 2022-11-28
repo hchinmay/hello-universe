187f8e44fda795f2e5dbc704fcfc2caa39c9936e
hchinmay <119351645+hchinmay@users.noreply.github.com>
2022-11-29 00:30:34 +0530

    hello.1

--- hellouni.cpp
+++ hellouni.cpp
@@ -3,6 +3,6 @@ using namespace std;
 int main()
 { 
   cout<<"hello universe";/*prints hello universe*/
-  cout<<"this is c++ programming practical";
+  cout<<"this is c++ programming  it practical";
   return 0;
 } 
